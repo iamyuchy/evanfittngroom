1725802faf62500ff2ad9cdb9033b483cc87de16
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-06 21:37:48 -0400

    first commit

---
+++ src/runtime/expressions.js
@@ -0,0 +1,11 @@
+var ls;
+(function (ls) {
+	ls.MainScene = function() {
+		return {
+			"%22selected%22": function() { return "selected" },
+			"%22initcat%22": function() { return "initcat" },
+			"%22init%22": function() { return "init" },
+			"%22equalTo%22": function() { return "equalTo" }
+		}
+	};
+})(ls || (ls = {}));
