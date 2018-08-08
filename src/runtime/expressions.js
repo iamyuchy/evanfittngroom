27d094b811849a412a7dcd6e444171a4825944d6
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-08 15:36:01 -0400

    add second set of clothes

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -2,10 +2,11 @@ var ls;
 (function (ls) {
 	ls.MainScene = function() {
 		return {
-			"%22selected%22": function() { return "selected" },
 			"%22initcat%22": function() { return "initcat" },
+			"%22equalTo%22": function() { return "equalTo" },
 			"%22init%22": function() { return "init" },
-			"%22equalTo%22": function() { return "equalTo" }
+			"%22selected%22": function() { return "selected" },
+			"%22select%22": function() { return "select" }
 		}
 	};
 })(ls || (ls = {}));
