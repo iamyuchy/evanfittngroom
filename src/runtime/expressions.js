54b0f2dc8095f16ba894f8beac8b59624620002e
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-08 17:10:59 -0400

    add multiple page function

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -3,9 +3,9 @@ var ls;
 	ls.MainScene = function() {
 		return {
 			"%22initcat%22": function() { return "initcat" },
-			"%22equalTo%22": function() { return "equalTo" },
 			"%22init%22": function() { return "init" },
 			"%22selected%22": function() { return "selected" },
+			"%22equalTo%22": function() { return "equalTo" },
 			"%22select%22": function() { return "select" }
 		}
 	};
