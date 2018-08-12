a199252c149b795ee1bafdcc0e18ee03b48202b3
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-11 22:27:28 -0400

    v2 outfit

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -2,10 +2,10 @@ var ls;
 (function (ls) {
 	ls.MainScene = function() {
 		return {
-			"%22selected%22": function() { return "selected" },
-			"%22init%22": function() { return "init" },
 			"%22initcat%22": function() { return "initcat" },
 			"%22equalTo%22": function() { return "equalTo" },
+			"%22init%22": function() { return "init" },
+			"%22selected%22": function() { return "selected" },
 			"%22select%22": function() { return "select" }
 		}
 	};
