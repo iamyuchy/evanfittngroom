8049c4561ea67cc88738728fb400326b2601afdb
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-13 11:49:12 -0400

    fix bug on FACE button

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -2,9 +2,9 @@ var ls;
 (function (ls) {
 	ls.MainScene = function() {
 		return {
-			"%22initcat%22": function() { return "initcat" },
-			"%22equalTo%22": function() { return "equalTo" },
 			"%22init%22": function() { return "init" },
+			"%22equalTo%22": function() { return "equalTo" },
+			"%22initcat%22": function() { return "initcat" },
 			"%22selected%22": function() { return "selected" },
 			"%22select%22": function() { return "select" }
 		}
