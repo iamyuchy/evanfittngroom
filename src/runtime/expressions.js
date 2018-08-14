f1dbaf883ab54e0ec2530bd0956003e9ffa6de26
Jiayun Liu <jiayun.liu@mail.utoronto.ca>
2018-08-13 23:52:35 -0400

    add 2 more outfit

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -2,10 +2,10 @@ var ls;
 (function (ls) {
 	ls.MainScene = function() {
 		return {
+			"%22selected%22": function() { return "selected" },
+			"%22initcat%22": function() { return "initcat" },
 			"%22init%22": function() { return "init" },
 			"%22equalTo%22": function() { return "equalTo" },
-			"%22initcat%22": function() { return "initcat" },
-			"%22selected%22": function() { return "selected" },
 			"%22select%22": function() { return "select" }
 		}
 	};
